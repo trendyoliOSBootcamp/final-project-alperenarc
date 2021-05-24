0715183f9e1d66d8dd21219016d5646e2d2e0b39
Alperen Arıcı <alparicieren@gmail.com>
2021-05-24 15:25:11 +0300

    Game app project initialized

---
+++ GameApp/GameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameApp
+//
+//  Created by Alperen Arıcı on 24.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
