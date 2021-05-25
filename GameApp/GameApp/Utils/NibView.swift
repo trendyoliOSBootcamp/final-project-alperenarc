4323a3d8ee825b3218710e8f2bdb6b62f77c2b20
Alperen Arıcı <alparicieren@gmail.com>
2021-05-25 17:42:31 +0300

    Platform Xib created

---
+++ GameApp/GameApp/Utils/NibView.swift
@@ -0,0 +1,8 @@
+//
+//  NibView.swift
+//  GameApp
+//
+//  Created by Alperen Arıcı on 24.05.2021.
+//
+
+import Foundation
---
+++ GameApp/GameApp/Views/Platform.swift
@@ -0,0 +1,20 @@
+//
+//  Platform.swift
+//  GameApp
+//
+//  Created by Alperen Arıcı on 24.05.2021.
+//
+
+import UIKit
+
+class Platform: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
